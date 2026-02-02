bb62bbc9f060d892bffc6df92981ae5f5cbd3f0d
Juan Pablo Enrici <jpenrici@gmail.com>
2026-02-02 18:33:18 -0300

    Studying SDL with Julia.

---
+++ Trainings/SDL/main.jl
@@ -0,0 +1,15 @@
+# main.jl
+
+function main()
+
+    @info "Starting..."
+
+    # TO DO
+
+    @info "Finished."
+    
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
---
+++ Trainings/SDL/scripts/dependencies.jl
@@ -0,0 +1,40 @@
+# dependencies.jl
+
+using Pkg
+
+
+function install_missing_packages()
+    try
+        required = ["SimpleDirectMediaLayer", "ColorTypes"]
+        project_deps = Pkg.project().dependencies
+
+        println("--- Checking Dependencies ---")
+        for pkg in required
+            if haskey(project_deps, pkg)
+                println("[SKIP] $pkg is already installed.")
+            else
+                println("[INST] Installing $pkg...")
+                Pkg.add(pkg)
+            end
+        end
+
+        println("--- Precompiling ---")
+        Pkg.precompile()
+
+        return true
+    catch e
+        @error "Failed to setup dependencies" exception=e
+        return false
+    end
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    Pkg.activate("..")
+    status = install_missing_packages()
+    if status
+        exit(0)
+    else
+        exit(1)
+    end
+end
